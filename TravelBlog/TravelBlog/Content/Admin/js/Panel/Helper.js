277b895d28479106d14908fce13e06b476e9c8fe
Hilal AKSOY <35233676+Hilalaksoy@users.noreply.github.com>
2020-01-03 21:39:15 +0300

    update

--- TravelBlog/TravelBlog/Content/Admin/js/Panel/Helper.js
+++
@@ -1,6 +0,0 @@
-﻿
-//function Info(id, tip, uyari) {
-//    var text = "<div class='alert alert-" + tip + " alert-dismissable'>" + "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>" + uyari + "</div>";
-//    $("#" + id).html(text);
-
-//}
